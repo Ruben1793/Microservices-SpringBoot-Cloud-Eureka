63721053f267fe937ed6c5e881a2557f82d6161d
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2020-03-29 15:41:02 -0600

    Tolerancia de fallos y latencia con Hystrix

--- springboot-servicio-items/src/main/java/com/formaciondbi/springboot/app/items/controllers/ItemController.java
+++ springboot-servicio-items/src/main/java/com/formaciondbi/springboot/app/items/controllers/ItemController.java
@@ -2,6 +2,8 @@ package com.formaciondbi.springboot.app.items.controllers;
 
 import com.formaciondbi.springboot.app.items.models.Item;
 import com.formaciondbi.springboot.app.items.models.ItemService;
+import com.formaciondbi.springboot.app.items.models.Producto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +24,22 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "alternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
         return itemService.findById(id, cantidad);
     }
+
+    public Item alternativo(Long id, Integer cantidad){
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return item;
+    }
+
 }
