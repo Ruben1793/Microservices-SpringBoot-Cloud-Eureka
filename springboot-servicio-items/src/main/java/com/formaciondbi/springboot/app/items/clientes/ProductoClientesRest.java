fd6f7eb65e92176ce78efba7a452169daf467948
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2020-05-05 17:02:22 -0500

    Consumiendo API Rest con Feign

--- springboot-servicio-items/src/main/java/com/formaciondbi/springboot/app/items/clientes/ProductoClientesRest.java
+++ springboot-servicio-items/src/main/java/com/formaciondbi/springboot/app/items/clientes/ProductoClientesRest.java
@@ -2,8 +2,7 @@ package com.formaciondbi.springboot.app.items.clientes;
 
 import com.formaciondbi.springboot.app.items.models.Producto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,4 +14,14 @@ public interface ProductoClientesRest {
 
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id);
+
+    @PostMapping("/crear")
+    public Producto crear(@RequestBody Producto producto);
+
+    @PutMapping("/editar/{id}")
+    public Producto update(@RequestBody Producto producto, @PathVariable Long id);
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable Long id);
+
 }
