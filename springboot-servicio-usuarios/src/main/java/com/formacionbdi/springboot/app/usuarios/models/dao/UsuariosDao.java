8473bd1f62e4a3ec1586f516bfed8532fd563831
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2020-06-27 19:41:49 -0600

    Probando la autenticacion con Postman y obteniendo el JWT token

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuariosDao.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuariosDao.java
@@ -4,10 +4,12 @@ import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuariosDao extends PagingAndSortingRepository<Usuario, Long> {
 
+    @RestResource(path = "buscar-username")
     public Usuario findByUsername(String username);
 
     @Query("select u from Usuario u where u.username = ?1")
